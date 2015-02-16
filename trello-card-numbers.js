fdbfc771255449ee1604d779a2607995ea500743
dewyze <john.dewyze@gmail.com>
2015-02-16 11:54:44 -0600

    Remove need for jquery

--- trello-card-numbers.js
+++ trello-card-numbers.js
@@ -1,94 +1,147 @@
 // ensure lightbox is loaded before adding to it
-function detailsReady(id) {
-    var dfd = jQuery.Deferred();
-
+function detailsReady() {
+  var promise = new Promise(function(resolve,reject) {
     var inc = 40;
     var detailsListener = function(interval) {
-      if ($(".window-title.card-detail-title.non-empty.inline.editable").length != 0) {
-            dfd.resolve($(".bt-card-details-number").length != 0);
+      var lightbox = document.getElementsByClassName("window-title card-detail-title non-empty inline editable");
+      if (lightbox.length != 0) {
+        resolve("true");
+      }
+      else {
+        interval = interval + 1 || 1;
+        if (interval < inc) {
+          setTimeout(function() { detailsListener(interval); }, 100);
         } else {
-            interval = interval + 1 || 1;
-            if (interval < inc) {
-                setTimeout(function() { detailsListener(interval); }, 100);
-            } else {
-                dfd.reject("Column Title timeout error");
-            }
+          reject("Lightbox Number Error");
         }
+      }
     };
     detailsListener();
+  });
 
-    return dfd.promise();
+  return promise;
 }
 
 // check that url has been added to card after it is created
 // this is done asynchronously a few ms later
 function hrefReady(obj) {
-    var dfd = jQuery.Deferred();
+  var promise = new Promise(function(resolve,reject) {
     var inc = 40;
     var hrefListener = function(interval) {
-      if (obj.attr("href") !== undefined) {
-            dfd.resolve(obj.attr("href"));
+      if (obj.getAttribute("href") != undefined) {
+        resolve(obj.getAttribute("href"));
+      } else {
+        interval = interval + 1 || 1;
+        if (interval < inc) {
+          setTimeout(function() { hrefListener(interval); }, 100);
         } else {
-            interval = interval + 1 || 1;
-            if (interval < inc) {
-                setTimeout(function() { hrefListener(interval); }, 100);
-            } else {
-
-                dfd.reject("Href timeout error");
-            }
+          reject("Href timeout error");
         }
+      }
     };
     hrefListener();
+  });
 
-    return dfd.promise();
+  return promise;
 }
 
 function log(data) {
-    console.log(data);
+  console.log(data);
 }
 
-$(document).ready(function() {
-    // show card numbers initially, this script is activated after everything is loaded
-    $(".list-header-num-cards").addClass("trello-card-numbers-inline-block");
-    $(".trello-card-numbers-inline-block").css("display","inline-block");
-    $(".card-short-id").addClass("trello-card-numbers-show");
-    $(".trello-card-numbers-show").show();
+function addClassToArray(arr,klass) {
+  var len = arr.length
+  for (var i=0; i < len; i++) {
+    var obj = arr[i];
+    obj.className = obj.className + " " + klass;
+  };
+}
 
-    // needed for switching boards
-    $(document).on("DOMNodeInserted",".list", function() {
-        $(".list-header-num-cards").addClass("trello-card-numbers-inline-block");
-        $(".trello-card-numbers-inline-block").css("display","inline-block");
-    })
+function addDisplayToArray(arr,style) {
+  var len = arr.length;
+  for (var i=0; i < len; i++) {
+    var obj = arr[i];
+    obj.style.display = style;
+  }
+}
 
-    // show card numbers after card is inserted
-    $(document).on("DOMNodeInserted",".list-card.js-member-droppable", function() {
-        $(".card-short-id").addClass("trello-card-numbers-show");
-        $(".trello-card-numbers-show").show();
+window.addEventListener("load", function() {
+  var listHeaders = document.getElementsByClassName("list-header-num-cards");
+  addClassToArray(listHeaders,"trello-card-numbers-inline-block");
+  listHeaders = document.getElementsByClassName("trello-card-numbers-inline-block");
+  addDisplayToArray(listHeaders,"inline-block");
 
-        // get card number for new cards using URL and add it to card-short-id
-        card = $(this).find("a.list-card-title.clear.js-card-name");
-        if (card.attr("href") === undefined) {
-            hrefReady($(this).find("a.list-card-title.clear.js-card-name")).then( function(href) {
-                var title = href.split("/");
-                var s = title[title.length-1];
-                var num = s.substr(0,s.indexOf("-"));
-                card.find(".card-short-id").html("#" + num + " ");
+  var shortIds = document.getElementsByClassName("card-short-id");
+  addClassToArray(shortIds,"trello-card-numbers-inline");
+  shortIds = document.getElementsByClassName("trello-card-numbers-inline");
+  addDisplayToArray(shortIds,"inline");
+
+  // needed for switching boards
+  // $(document).on("DOMNodeInserted",".list", function() {
+    // $(".list-header-num-cards").addClass("trello-card-numbers-inline-block");
+    // $(".trello-card-numbers-inline-block").css("display","inline-block");
+  // })
+
+  // show card numbers after card is inserted
+  var target = document.querySelector("#content");
+
+  var config = { attributes: true, childList: true, subtree: true, characterData: true }
+
+  var observer = new MutationObserver(function(mutations) {
+    mutations.forEach(function(mutation) {
+      if (mutation.addedNodes.length > 0) {
+        var node = mutation.addedNodes[0];
+        var classes = node.classList;
+        if (classes && classes.contains("list-card") && classes.contains("js-member-droppable")) {
+          var shortIds = document.getElementsByClassName("card-short-id");
+          addClassToArray(shortIds,"trello-card-numbers-inline");
+          shortIds = document.getElementsByClassName("trello-card-numbers-inline");
+          addDisplayToArray(shortIds,"inline");
+          var card = node.querySelectorAll("a.list-card-title.clear.js-card-name")[0];
+          if (card.getAttribute("href") == undefined) {
+            hrefReady(card).then(function(href) {
+              var title = href.split("/");
+              var s = title[title.length-1];
+              var num = s.substr(0,s.indexOf("-"));
+              var shortId = card.querySelector(".card-short-id");
+              shortId.innerHTML = "#" + num + " ";
+            }, function(err) {
+              log(err);
             });
+          }
         }
+      }
     });
+  });
 
-    // add card number to card details lightbox
-    $("body").on("click mouseup","div.list-card-details.u-clearfix", function() {
-        var id = $(this).find(".card-short-id").html();
-        detailsReady(id).then(function() {
+  observer.observe(target,config);
+  // });
 
-            // if/else needed to handle multiple promises
-            var obj = $(".window-title.card-detail-title.non-empty.inline.editable");
-            if ($(".trello-card-numbers-detail-header").length > 0) {
-                $(".trello-card-numbers-detail-header").html(id);
-            } else {
-                obj.prepend("<h2 class='trello-card-numbers-detail-header quiet'>" + id + "</h2>");
-            }
-        });
-    });
-});
+  // $(document).on("DOMNodeInserted",".list-card.js-member-droppable", function() {
+
+  //   // get card number for new cards using URL and add it to card-short-id
+  // });
+
+  // add card number to card details lightbox
+  document.body.addEventListener("mouseup", function(e) {
+    if (
+      e.target.getAttribute('class').indexOf('list-card-details') >= 0
+      || e.target.parentNode.getAttribute('class').indexOf('list-card-details') >= 0
+    ) {
+      var id = e.target.querySelectorAll(".card-short-id")[0].innerHTML;
+      detailsReady().then(function() {
+
+        // if/else needed to handle multiple promises
+        var header = document.getElementsByClassName("trello-card-numbers-detail-header");
+        if (header.length > 0) {
+          header.innerHTML = id;
+        } else {
+          var obj = document.getElementsByClassName("window-title card-detail-title non-empty inline editable")[0];
+          obj.innerHTML = "<h2 class='trello-card-numbers-detail-header quiet'>" + id + "</h2>" + obj.innerHTML;
+        }
+      }, function (err) {
+        log(err);
+      });
+    }
+  }, true);
+}, false);
