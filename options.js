35b922883f28fb30a073a4ca57ba5e50882b5abe
John DeWyze <john.dewyze@gmail.com>
2015-03-25 17:39:41 -0400

    Copy card id right click option added.

--- options.js
+++ options.js
@@ -1,8 +1,10 @@
 // Saves options to chrome.storage.sync.
 function save_options() {
     var bold = document.getElementById('bold').checked;
+    var copy = document.getElementById('copy').checked;
     chrome.storage.sync.set({
-        boldId: bold
+        boldId: bold,
+        rightClickCopy: copy
     }, function() {
         // Update status to let user know options were saved.
         var status = document.getElementById('status');
@@ -19,8 +21,10 @@ function restore_options() {
     // Use default value color = 'red' and likesColor = true.
     chrome.storage.sync.get({
         boldId: false,
+        rightClickCopy: true
     }, function(items) {
         document.getElementById('bold').checked = items.boldId;
+        document.getElementById('copy').checked = items.rightClickCopy;
     });
 }
 
