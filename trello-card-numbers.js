e42bebe255f8382ef1b382bbd54b698d16a42bb9
John DeWyze <john.dewyze@gmail.com>
2017-01-20 10:25:01 -0600

    Convert double quotes to single quotes and rename detailsReady to mention lightbox

--- trello-card-numbers.js
+++ trello-card-numbers.js
@@ -1,32 +1,32 @@
-var LIGHTBOX_SELECTOR = "window-title";
-var CARD_LINK_QUERY_SELECTOR = "a.list-card-title.js-card-name";
-var LIST_NUM_CARDS_CLASS = "list-header-num-cards";
-var CARD_SHORT_ID = "card-short-id";
-var CARD_SHORT_ID_SELECTOR = "." + CARD_SHORT_ID
-var SEARCH_RESULT_CARD = "search-result-card";
-var TCN_HEADER = "trello-card-numbers-detail-header";
-var TCN_INLINE = "trello-card-numbers-inline";
-var TCN_INLINE_BLOCK = "trello-card-numbers-inline-block";
+var LIGHTBOX_SELECTOR = 'window-title';
+var CARD_LINK_QUERY_SELECTOR = 'a.list-card-title.js-card-name';
+var LIST_NUM_CARDS_CLASS = 'list-header-num-cards';
+var CARD_SHORT_ID = 'card-short-id';
+var CARD_SHORT_ID_SELECTOR = '.' + CARD_SHORT_ID
+var SEARCH_RESULT_CARD = 'search-result-card';
+var TCN_HEADER = 'trello-card-numbers-detail-header';
+var TCN_INLINE = 'trello-card-numbers-inline';
+var TCN_INLINE_BLOCK = 'trello-card-numbers-inline-block';
 
 // ensure lightbox is loaded before adding to it
-function detailsReady() {
+function lightboxReady() {
     var promise = new Promise(function(resolve,reject) {
         var inc = 40;
-        var detailsListener = function(interval) {
+        var lightboxListener = function(interval) {
             var lightbox = document.getElementsByClassName(LIGHTBOX_SELECTOR);
             if (lightbox.length != 0) {
-                resolve("true");
+                resolve('true');
             }
             else {
                 interval = interval + 1 || 1;
                 if (interval < inc) {
-                    setTimeout(function() { detailsListener(interval); }, 100);
+                    setTimeout(function() { lightboxListener(interval); }, 100);
                 } else {
-                    reject("Lightbox Timeout");
+                    reject('Lightbox Timeout');
                 }
             }
         };
-        detailsListener();
+        lightboxListener();
     });
 
     return promise;
@@ -38,14 +38,14 @@ function hrefReady(obj) {
     var promise = new Promise(function(resolve,reject) {
         var inc = 40;
         var hrefListener = function(interval) {
-            if (obj.getAttribute("href") != undefined) {
-                resolve(obj.getAttribute("href"));
+            if (obj.getAttribute('href') != undefined) {
+                resolve(obj.getAttribute('href'));
             } else {
                 interval = interval + 1 || 1;
                 if (interval < inc) {
                     setTimeout(function() { hrefListener(interval); }, 100);
                 } else {
-                    reject("Href timeout error");
+                    reject('Href timeout error');
                 }
             }
         };
@@ -60,7 +60,7 @@ function addClassToArray(arr,klass) {
     for (var i=0; i < len; i++) {
         var obj = arr[i];
         if (!hasClass(obj, klass)) {
-            obj.className = obj.className + " " + klass;
+            obj.className = obj.className + ' ' + klass;
         }
     };
 }
@@ -74,11 +74,11 @@ function addStyleToArray(arr,attribute,style) {
 }
 
 function boldifyCardids() {
-    arr = getByClass("trello-card-numbers-inline");
+    arr = getByClass('trello-card-numbers-inline');
     var len = arr.length;
     for (var i=0; i < len; i++) {
         var obj = arr[i];
-        obj.style.fontWeight = "bold";
+        obj.style.fontWeight = 'bold';
     }
 }
 
@@ -87,14 +87,14 @@ function addClassWithDisplay(selector, newClass, display, callback) {
         var objects = getByClass(selector);
         addClassToArray(objects, newClass);
         objects = getByClass(newClass);
-        addStyleToArray(objects, "display", display);
+        addStyleToArray(objects, 'display', display);
         chrome.storage.sync.get(function(items) {
             if (selector == CARD_SHORT_ID) {
                 if (items.boldId) {
-                    addStyleToArray(objects, "fontWeight", "bold");
+                    addStyleToArray(objects, 'fontWeight', 'bold');
                 }
                 if (items.idColor) {
-                    addStyleToArray(objects, "color", "#" + items.idColor);
+                    addStyleToArray(objects, 'color', '#' + items.idColor);
                 }
             }
         });
@@ -109,15 +109,15 @@ function addTrailingSpace(selector) {
     var len = objects.length
     for (var i=0; i < len; i++) {
         var obj = objects[i];
-        obj.innerHTML = obj.innerHTML + " ";
+        obj.innerHTML = obj.innerHTML + ' ';
     };
 }
 
 
 function hasClass(target, className) {
-    className = " " + className + " ";
+    className = ' ' + className + ' ';
     if (target.className) {
-        return (" " + target.className + " ").replace(/[\n\t]/g, " ").indexOf(className) > -1
+        return (' ' + target.className + ' ').replace(/[\n\t]/g, ' ').indexOf(className) > -1
     }
     return false;
 }
@@ -128,11 +128,11 @@ function getByClass(name) {
 
 function getAncestorBySelector(elem, selector) {
     var node = elem;
-    while (node.tagName != "BODY") {
+    while (node.tagName != 'BODY') {
         if (hasClass(node, selector)) {
             return node;
         }
-        if (node.parentNode !== "undefined") {
+        if (node.parentNode !== 'undefined') {
             node = node.parentNode;
         } else {
             return null;
@@ -144,11 +144,11 @@ window.addEventListener("load", function() {
     var showListNumbers = addClassWithDisplay(LIST_NUM_CARDS_CLASS, TCN_INLINE_BLOCK, "inline-block", null);
     showListNumbers();
     // addTrailingSpace(CARD_SHORT_ID);
-    var showCardIds = addClassWithDisplay(CARD_SHORT_ID, TCN_INLINE, "inline", addTrailingSpace);
+    var showCardIds = addClassWithDisplay(CARD_SHORT_ID, TCN_INLINE, 'inline', addTrailingSpace);
     showCardIds();
 
     // show card numbers after card is inserted
-    var target = document.querySelector("body");
+    var target = document.querySelector('body');
     var config = { attributes: true, childList: true, subtree: true, characterData: true }
     var observer = new MutationObserver(function(mutations) {
         mutations.forEach(function(mutation) {
@@ -159,25 +159,25 @@ window.addEventListener("load", function() {
                     if (hasClass(node, SEARCH_RESULT_CARD) || hasClass(node, CARD_SHORT_ID)) {
                         showCardIds();
                     }
-                    else if (hasClass(node, "list-card") && hasClass(node, "js-member-droppable")) {
+                    else if (hasClass(node, 'list-card') && hasClass(node, 'js-member-droppable')) {
                         showCardIds();
                         var card = node.querySelectorAll(CARD_LINK_QUERY_SELECTOR)[0];
                         var duplicateCheck = node.querySelectorAll(CARD_SHORT_ID_SELECTOR).length > 0;
-                        if (card.getAttribute("href") == undefined && !duplicateCheck) {
+                        if (card.getAttribute('href') == undefined && !duplicateCheck) {
                             hrefReady(card).then(function(href) {
                                 var title = href.split("/");
                                 var s = title[title.length-1];
                                 var num = s.substr(0,s.indexOf("-"));
                                 var cardTitle = card.innerHTML;
-                                var shortId = document.createElement("span");
-                                shortId.innerHTML = "#" + num + " ";
-                                shortId.className = "card-short-id hide trello-card-numbers-inline trello-card-numbers-inline";
+                                var shortId = document.createElement('span');
+                                shortId.innerHTML = '#' + num + ' ';
+                                shortId.className = 'card-short-id hide trello-card-numbers-inline trello-card-numbers-inline';
                                 card.insertBefore(shortId, card.firstChild);
                             }, function(err) {
                                 console.error(err);
                             });
                         }
-                    } else if (classes.contains("list")) {
+                    } else if (classes.contains('list')) {
                         showListNumbers();
                     }
                 }
@@ -189,7 +189,7 @@ window.addEventListener("load", function() {
 
     // add card number to card details lightbox
     var id; // must set outside so each click overwrites the last id stored
-    document.body.addEventListener("mouseup", function(e) {
+    document.body.addEventListener('mouseup', function(e) {
         var listCard =  getAncestorBySelector(e.target, 'list-card-details') || getAncestorBySelector(e.target, SEARCH_RESULT_CARD);
         if (listCard) {
             var cardId = listCard.querySelectorAll(CARD_SHORT_ID_SELECTOR)[0];
