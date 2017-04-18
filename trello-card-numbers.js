62a7546a5c950b8b7b39ea25c2d71754b7c3d35b
Brad Moore <brad@4pi.com.au>
2017-04-18 23:10:46 +1000

    Updated to support copy details button.

--- trello-card-numbers.js
+++ trello-card-numbers.js
@@ -161,11 +161,38 @@ function addNumberToLightboxWhenReady(cardNumber) {
             h2.style.marginRight = '10px';
             h2.innerHTML = '<span>' + cardNumber + '</span>';
             obj.insertBefore(h2, obj.lastChild);
+
+
+            chrome.storage.sync.get(function(items) {
+                // undefined if not set. Assume enabled by default.
+                if (items.showCopy == undefined || items.showCopy == true) {
+                    var copyButton = getByClass("button-link js-copy-card")[0];
+
+                    var copyDetailsButton = document.createElement('a');
+                    copyDetailsButton.className = 'button-link';
+                    copyDetailsButton.href = '#';
+                    copyDetailsButton.onclick = function() {
+                        var cardText = getByClass('js-card-detail-title-input')[0].value;
+                
+                        // Ew....
+                        // Source http://stackoverflow.com/a/18455088
+                        var copyFrom = document.createElement("textarea");
+                        copyFrom.textContent = cardNumber.trim() + ", " + cardText; // Unsure if its ok to refer to cardNumber from params.
+                        document.body.appendChild(copyFrom);
+                        copyFrom.select();
+                        document.execCommand('copy');
+                        document.body.removeChild(copyFrom);
+                    };
+                    copyDetailsButton.innerHTML = '<span class="icon-sm icon-card"></span>&nbsp;Copy details</a>';
+                    copyButton.parentNode.insertBefore(copyDetailsButton, copyButton.nextSibling); 
+                }
+            });       
         }
     }, function (err) {
         null;
     });
 }
+
 
 window.addEventListener('load', function() {
     var showListNumbers = addClassWithDisplay(LIST_NUM_CARDS_CLASS, TCN_INLINE_BLOCK, 'inline-block', null);
