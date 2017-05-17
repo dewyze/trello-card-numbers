7d3af921d3e768f0cab8eacde1b670f82ad81f08
John DeWyze <john.dewyze@gmail.com>
2017-05-16 21:15:35 -0500

    Reload ids after 1 second when new board is clicked

--- trello-card-numbers.js
+++ trello-card-numbers.js
@@ -7,6 +7,8 @@ var SEARCH_RESULT_CARD = 'search-result-card';
 var TCN_HEADER = 'trello-card-numbers-detail-header';
 var TCN_INLINE = 'trello-card-numbers-inline';
 var TCN_INLINE_BLOCK = 'trello-card-numbers-inline-block';
+var BOARD_URL_REGEX = /trello\.com\/b\//;
+var CARD_URL_REGEX = /trello\.com\/c\//;
 
 // ensure lightbox is loaded before adding to it
 function lightboxReady() {
@@ -140,6 +142,20 @@ function getAncestorBySelector(elem, selector) {
     }
 }
 
+function getParentLink(elem) {
+    var node = elem;
+    while (node.tagName != 'BODY') {
+        if (node.tagName === 'A') {
+            return node.href;
+        }
+        if (node.parentNode !== 'undefined') {
+            node = node.parentNode;
+        } else {
+            return null;
+        }
+    }
+}
+
 function getCardNumberFromUrl(url) {
     var title = url.split('/');
     var s = title[title.length-1];
@@ -191,11 +207,15 @@ function addNumberToLightboxWhenReady(cardNumber) {
     });
 }
 
+function urlMatch(regex, url) {
+    if (url === undefined) { return false };
+    var matches = url.match(regex);
+    return matches != null && matches.length !== 0;
+}
 
 window.addEventListener('load', function() {
     var showListNumbers = addClassWithDisplay(LIST_NUM_CARDS_CLASS, TCN_INLINE_BLOCK, 'inline-block', null);
     showListNumbers();
-    // addTrailingSpace(CARD_SHORT_ID);
     var showCardIds = addClassWithDisplay(CARD_SHORT_ID, TCN_INLINE, 'inline', addTrailingSpace);
     showCardIds();
 
@@ -210,8 +230,7 @@ window.addEventListener('load', function() {
                 if (node.classList) {
                     if (hasClass(node, SEARCH_RESULT_CARD) || hasClass(node, CARD_SHORT_ID)) {
                         showCardIds();
-                    }
-                    else if (hasClass(node, 'list-card') && hasClass(node, 'js-member-droppable')) {
+                    } else if (hasClass(node, 'list-card') && hasClass(node, 'js-member-droppable')) {
                         showCardIds();
                         var card = node.querySelectorAll(CARD_LINK_QUERY_SELECTOR)[0];
                         var duplicateCheck = node.querySelectorAll(CARD_SHORT_ID_SELECTOR).length > 0;
@@ -239,6 +258,11 @@ window.addEventListener('load', function() {
     // add card number to card details lightbox
     var id; // must set outside so each click overwrites the last id stored
     document.body.addEventListener('mouseup', function(e) {
+        var link = getParentLink(e.target);
+        if (urlMatch(BOARD_URL_REGEX, link) && link !== window.location.href) {
+            setTimeout(function() { showListNumbers(); showCardIds(); }, 1000);
+        }
+
         var listCard =  getAncestorBySelector(e.target, 'list-card-details') || getAncestorBySelector(e.target, SEARCH_RESULT_CARD);
         if (listCard) {
             var cardId = listCard.querySelectorAll(CARD_SHORT_ID_SELECTOR)[0];
@@ -249,10 +273,9 @@ window.addEventListener('load', function() {
         }
     }, true);
 
-    var cardUrlRegex = /trello\.com\/c\//;
+
     var pageUrl = document.location.href;
-    var matches = pageUrl.match(cardUrlRegex);
-    if (matches != null && matches.length !== 0) {
+    if (urlMatch(CARD_URL_REGEX, pageUrl)) {
         var num = '#' + getCardNumberFromUrl(pageUrl);
         addNumberToLightboxWhenReady(num);
     }
